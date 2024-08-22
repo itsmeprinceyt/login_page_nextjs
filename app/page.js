014311c7a6d5a085dce0fb2b0b935bd9faa4f9a8
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-22 22:52:29 +0530

    working on ui

--- app/page.js
+++ app/page.js
@@ -6,7 +6,13 @@ export default function Home() {
   return (
     <div className="min-h-screen text-white flex flex-col justify-center items-center">
       <div className="main-content z-10 flex flex-col justify-around items-center h-[380px]  bg-white border text-black border-gray-100/10 shadow-xl shadow-black/40 p-10 gap-6 rounded-xl">
-        hello
+        <div className="bg-green-400  flex flex-col items-center justify-center">
+          <div className="font-bold text-4xl">Hi, Welcome back!</div>
+          <div>Hope you&apos;re doing fine!</div>
+        </div>
+        <div>
+          Box
+        </div>
       </div>
       {/*Background Image*/}
       <Image
